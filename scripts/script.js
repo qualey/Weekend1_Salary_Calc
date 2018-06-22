304e83defcbc0fbd9e503f5cff503a06e9c9f792
Ross Qualey <qualeyro@gmail.com>
2018-06-22 16:53:34 -0500

    formatting mostly complete and files are set up

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,21 @@
 console.log('JS');
 
+const employees = [
+    {
+        firstName : '',
+        lastName : '',
+        idNumber : '',
+        jobTitle : '',
+        annualSalary : '',
+    },{
+        firstName : '',
+        lastName : '',
+        idNumber : '',
+        jobTitle : '',
+        annualSalary : '',
+    }
+];
+
 $(document).ready( readyNow );
 function readyNow() {
     console.log('JQ');
